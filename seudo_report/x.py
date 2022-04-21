c285cd012817e3ff28237ecb70f590b0ed8d8bfa
Ethan Wang <waxinyu@umich.edu>
2022-04-21 07:23:02 -0400

    add some seudocode

---
+++ seudo_report/x.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+def discretize(state):
+    return cell
+
+
+
+# known state and goal, but we avoid using heuristic
+# discretize the continuous space, called cells
+
+# In SoRB, a goal-conditioned value function provides edge weights, and nodes are taken to be previously seen observations in a replay buffer
+
+
+# Try Restoring simulator state is a highly efficient method for returning to previously visited states?
